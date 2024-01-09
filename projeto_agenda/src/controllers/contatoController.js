c7cd72d79dd6d530c7652b83898783785be86637
Leonardo Santos <leonardojosantos@gmail.com>
2024-01-09 20:13:49 -0300

    Class 156 - Editing Contacts (Update)

--- projeto_agenda/src/controllers/contatoController.js
+++ projeto_agenda/src/controllers/contatoController.js
@@ -1,4 +1,3 @@
-// 
 const Contato = require('../models/ContatoModel');
 
 exports.index = (req, res) => {
@@ -13,13 +12,11 @@ exports.register = async (req, res) => {
         if (contato.errors.length > 0) {
             req.flash('errors', contato.errors);
             req.session.save(() => res.redirect('/contato/index'));
-            // req.session.save(() => res.redirect('back'));
             return;
         }
 
         req.flash('success', 'Contato registrado com sucesso!');
         req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
-        // req.session.save(() => res.redirect('back'));
         return;
     }
     catch (e) {
@@ -36,4 +33,26 @@ exports.editIndex = async function (req, res) {
     if (!contato) return res.render('404');
 
     res.render('contato', { contato });
+};
+
+exports.edit = async function (req, res) {
+    try {
+        if (!req.params.id) return res.render('404');
+        const contato = new Contato(req.body);
+        await contato.edit(req.params.id);
+
+        if (contato.errors.length > 0) {
+            req.flash('errors', contato.errors);
+            req.session.save(() => res.redirect(`/contato/index/${req.params.id}`));
+            return;
+        }
+
+        req.flash('success', 'Contato editado com sucesso!');
+        req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
+        return;
+    }
+    catch (error) {
+        console.log(error);
+        res.render('404');
+    }
 };
